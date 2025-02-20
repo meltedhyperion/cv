9aee1898beb069ffee2e92a0c9fbb7d90451bdd0
meltedhyperion <aryan12official34@gmail.com>
2025-02-20 11:40:25 +0530

    remove logos

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,11 +1,3 @@
-import {
-  ClevertechLogo,
-  ConsultlyLogo,
-  JojoMobileLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-} from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
@@ -62,7 +54,6 @@ export const RESUME_DATA = {
       link: "https://www.isro.gov.in/",
       badges: ["CV", "DL","FPGA","SLAM", "ROS"],
       title: "Research Intern",
-      logo: NSNLogo,
       start: "Aug, 2024",
       end: "Dec, 2024",
       description: (
@@ -87,7 +78,6 @@ export const RESUME_DATA = {
       link: "https://mable.ai",
       badges: ["Remote", "GoLang", "TS","Python", "PostgreSQL", "AWS", "Redis"],
       title: "Backend Developer (Part Time)",
-      logo: ConsultlyLogo,
       start: "Oct, 2022",
       end: "Dec, 2023",
       description: (
@@ -115,7 +105,6 @@ export const RESUME_DATA = {
       link: "https://www.iitr.ac.in/",
       badges: ["Computer Vision", "Embedded Systems", "C", "RPi", "Python"],
       title: "Research Intern",
-      logo: ConsultlyLogo,
       start: "Jun, 2023",
       end: "Jul, 2023",
       description: (
@@ -140,7 +129,6 @@ export const RESUME_DATA = {
       link: "https://srmkzilla.net/",
       badges: ["GoLang", "Node.js", "MongoDB", "AWS"],
       title: "Technical Lead",
-      logo: MonitoLogo,
       start: "Oct, 2021",
       end: "May, 2024",
       description: (
@@ -177,7 +165,6 @@ export const RESUME_DATA = {
       techStack: ["GoLang", "Python", "MongoDB", "AWS", "kestra.io"],
       description:
         "A data injection pipeline that automates the task of data association between soource and target schema.",
-      logo: MonitoLogo,
       link: {
         label: "sdi.aryansingh.dev",
         href: "https://github.com/meltedhyperion/smart-data-injector",
@@ -194,7 +181,6 @@ export const RESUME_DATA = {
       ],
       description:
         "An Effective Landmark Regression Using Attention Based-HRNet For Satellite Pose Estimation",
-      logo: ConsultlyLogo,
       link: {
         label: "isro.org.in",
         href: "https://drive.google.com/file/d/1Ja5hg57V1hBD2ZCm526zYRDHCtdvwApK/view?usp=sharing",
@@ -205,7 +191,6 @@ export const RESUME_DATA = {
       techStack: ["GoLang", "Remix", "MongoDB", "AWS"],
       description:
         "A recruitment portal for campus clubs with secure and seamless account creation using college email integration",
-      logo: MonitoLogo,
       link: {
         label: "recruitment.srmkzilla.net",
         href: "github.com/meltedhyperion/fury",
@@ -216,7 +201,6 @@ export const RESUME_DATA = {
       techStack: ["Express", "React", "MongoDB"],
       description:
         "A browser extension to simplify exploration of open-source projects on GitHub based on user skills and project community.",
-      logo: MonitoLogo,
       link: {
         label: "recruitment.srmkzilla.net",
         href: "github.com/meltedhyperion/fury",
